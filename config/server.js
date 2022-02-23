ea2b6d90dd2d81a6c4ab93fd2a782b9c83f9b7ab
UlysseGebe <Uly35534e31>
2022-02-23 14:40:17 +0100

    Init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '43c664ded174acb0e15c62217195062d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '689fcb7c-6282-4298-9894-1337d81ba593'
+};
