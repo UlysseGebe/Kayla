2d3855ec001f5859e47e2c0cef49b6143daba124
UlysseGebe <Uly35534e31>
2022-02-24 17:45:42 +0100

    Fix

--- config/database.js
+++ config/database.js
@@ -1,13 +1,12 @@
-const parse = require('pg-connection-string').parse;
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: parse(env('DATABASE_URL', 'postgres://ghbrndvgletnnl:b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/dnq9jpjdrosfn')).host,
-      port: parse(env('DATABASE_URL', 'postgres://ghbrndvgletnnl:b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/dnq9jpjdrosfn')).port,
-      database: parse(env('DATABASE_URL', 'postgres://ghbrndvgletnnl:b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/dnq9jpjdrosfn')).database,
-      user: parse(env('DATABASE_URL', 'postgres://ghbrndvgletnnl:b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/dnq9jpjdrosfn')).user,
-      password: parse(env('DATABASE_URL', 'postgres://ghbrndvgletnnl:b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5@ec2-54-216-17-9.eu-west-1.compute.amazonaws.com:5432/dnq9jpjdrosfn')).password,
+      host: 'ec2-54-216-17-9.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      database: 'dnq9jpjdrosfn',
+      user: 'ghbrndvgletnnl',
+      password: 'b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5',
       ssl: {
         rejectUnauthorized: false
       },
