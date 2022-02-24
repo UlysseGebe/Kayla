3dab101b512789df3b493de8f1bec6442de6e12d
UlysseGebe <Uly35534e31>
2022-02-24 15:55:34 +0100

    Add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
-      >.
+      >
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
@@ -96,6 +96,155 @@
           target="_blank"
           rel="noopener"
           >awesome-vue</a
+        >
+      </li>
+    </ul>
+    <h3>Lien utiles pour la création de l'app</h3>
+    <ul>
+      <li>
+        <a
+          href="https://hjkcai.github.io/vue-modal-dialogs/"
+          target="_blank"
+          rel="noopener"
+          >Modales</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/hjl19911127/vue-drawer-layout"
+          target="_blank"
+          rel="noopener"
+          >Menu sur le coté</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/VitorLuizC/vue-burger-button"
+          target="_blank"
+          rel="noopener"
+          >Burger bouton</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/surmon-china/vue-awesome-swiper"
+          target="_blank"
+          rel="noopener"
+          >Slider 1</a
+        >
+      </li>
+      <li>
+        <a href="https://swiperjs.com/vue" target="_blank" rel="noopener"
+          >Slider 2</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/bedlate/vue-data-loading"
+          target="_blank"
+          rel="noopener"
+          >Scroll infini avec chargement 1</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/duyanpeng/vue-quick-loadmore"
+          target="_blank"
+          rel="noopener"
+          >Scroll infini avec chargement 2</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/nicolasbeauvais/vue-social-sharing"
+          target="_blank"
+          rel="noopener"
+          >Liens réseaux 1</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/Alexandrshy/vue-share-buttons"
+          target="_blank"
+          rel="noopener"
+          >Liens réseaux 2</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/scopewu/qrcode.vue"
+          target="_blank"
+          rel="noopener"
+          >Générateur de QR code</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/HJ29/vue3-qr-reader"
+          target="_blank"
+          rel="noopener"
+          >Lecteur de QR code</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://www.algolia.com/doc/guides/building-search-ui/what-is-instantsearch/vue/"
+          target="_blank"
+          rel="noopener"
+          >Recherche avec barre et critères</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/mdbootstrap/mdb-vue-ui-kit"
+          target="_blank"
+          rel="noopener"
+          >Material Design Bootstrap</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://pgbross.github.io/vue-material-adapter/#/component/button"
+          target="_blank"
+          rel="noopener"
+          >Vuejs Material Design Android</a
+        >
+      </li>
+      <li>
+        <a href="https://bootstrap-vue.org/" target="_blank" rel="noopener"
+          >Bootstrap pour Vuejs</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://v4.apollo.vuejs.org/guide/installation.html#compatibility"
+          target="_blank"
+          rel="noopener"
+          >GraphQL avec Apollo</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://vuex-orm.github.io/plugin-graphql/guide/#actions"
+          target="_blank"
+          rel="noopener"
+          >GraphQL avec Orm (VueX)</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/cocoastorm/vue-paypal-checkout"
+          target="_blank"
+          rel="noopener"
+          >Paiement Paypal</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vue-stripe/vue-stripe"
+          target="_blank"
+          rel="noopener"
+          >Paiement Stripe</a
         >
       </li>
     </ul>
