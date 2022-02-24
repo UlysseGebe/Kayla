dc111229ca3ac163f6f2a71c917c2361d9d39061
UlysseGebe <Uly35534e31>
2022-02-24 17:25:48 +0100

    Test

--- config/env/production/server.js
+++
@@ -1,7 +0,0 @@
-module.exports = ({ env }) => ({
-    url: env("https://kayla-project.herokuapp.com/"),
-    proxy: true,
-    app: {
-      keys: env.array("APP_KEYS", ["DATABASE_URL"]),
-    },
-});
--- config/server.js
+++ config/server.js
@@ -1,9 +1,14 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('API_URL', "https://kayla-project.herokuapp.com/"),
   admin: {
+    url: env('ADMIN_URL', "https://kayla-project.herokuapp.com/admin"), 
     auth: {
       secret: env('ADMIN_JWT_SECRET', '43c664ded174acb0e15c62217195062d'),
     },
+  },
+  app: {
+    keys: env.array("APP_KEYS", ["DATABASE_URL"]),
   },
 });
