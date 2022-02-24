5327dd5694dbd16b06b9e42dbce39a489a964d51
UlysseGebe <Uly35534e31>
2022-02-24 17:56:04 +0100

    Fix

--- config/database.js
+++ config/database.js
@@ -1,16 +1,20 @@
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: 'ec2-54-216-17-9.eu-west-1.compute.amazonaws.com',
-      port: 5432,
-      database: 'dnq9jpjdrosfn',
-      user: 'ghbrndvgletnnl',
-      password: 'b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5',
-      ssl: {
-        rejectUnauthorized: false
-      },
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "ec2-54-216-17-9.eu-west-1.compute.amazonaws.com"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "dnq9jpjdrosfn"),
+        username: env("DATABASE_USERNAME", "ghbrndvgletnnl"),
+        password: env("DATABASE_PASSWORD", "b613a3308fc834241439803358247697b31b253381ca89e130febf96b230afc5"),
+        schema: env("DATABASE_SCHEMA", "public"),
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
     },
-    debug: false,
   },
 });
